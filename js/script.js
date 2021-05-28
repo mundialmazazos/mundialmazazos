7fd22f4801dd07ba2e5efc64784666d8c9d59001
mundialmazazos <84957334+mundialmazazos@users.noreply.github.com>
2021-05-28 01:37:30 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 30, 2021 23:30:00 GMT+02:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "<p class=''>Estamos en vivo: </p>  <div class='redes-sociales'> <a href='https://www.twitch.tv/borja_zazo' class='twitch'><i class='fa fa-twitch'></i></a><div>";
+  }
+}, 1000);
