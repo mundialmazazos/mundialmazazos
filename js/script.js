c9f37077041bab59f3c331e7707c239ea1197ecb
mundialmazazos <84957334+mundialmazazos@users.noreply.github.com>
2021-05-31 05:55:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 30, 2021 23:30:00 GMT+02:00").getTime();
+var countDownDate = new Date("Jun 2, 2021 21:00:00 GMT+02:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
